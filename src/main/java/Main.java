0caf4c2c033a3f5fc4e808998dfa887c40d29fc1
David-Aldair <d.aldair.quinonez@gmail.com>
2025-10-28 22:40:41 -0400

    Instances for each type of vehicle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        //moped instance
         Moped slowRide = new Moped();
         slowRide.setColor("Red");
         slowRide.setFuelCapacity(5);
+
+        //hovercraft instance
+        Hovercraft hotRide = new Hovercraft();
+        hotRide.setColor("Blue");
+        hotRide.setFuelCapacity(10);
+
+        //car instance
+        Car fordF150 = new Car();
+        fordF150.setColor("Blue");
+        fordF150.setFuelCapacity(35);
+
+        //semitruck instance
+        SemiTruck oldFaithful = new SemiTruck();
+        oldFaithful.setColor("Hot Red");
+        oldFaithful.setFuelCapacity(50);
+
 
     }
 }
